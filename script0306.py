f9329c72453dc5661c5a81cdfbed02bc4fab2716
ttjin21 <dldbwls825@gmail.com>
2025-03-06 11:26:03 +0900

    Add files via upload

---
+++ script0306.py
@@ -0,0 +1,26 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello World!')
+Hello World!
+>>> print('Hello', 'World!')
+Hello World!
+>>> print('Hello'+'World!')
+HelloWorld!
+>>> print(100)
+100
+>>> print(2+4)
+6
+>>> print(351458+7875)
+359333
+>>> 100
+100
+>>> 10+20
+30
+>>> 1587+5666
+7253
+>>> 10/5
+2.0
+>>> 10*25
+250
+>>> 15*52+45*2
+870
