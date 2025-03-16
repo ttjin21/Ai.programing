836ffc56b78daca411e0d06e19fa1df1c9056034
ttjin21 <dldbwls825@gmail.com>
2025-03-17 03:37:55 +0900

    Add files via upload

---
+++ script0318_2.py
@@ -0,0 +1,31 @@
+Python 3.11.0b1 (main, May  7 2022, 22:43:46) [MSC v.1931 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+celsius=int(input('섭씨 온도를 입력하세요:'))
+섭씨 온도를 입력하세요:30
+fahrenheit=(9/5)*celsius+32
+print('섭씨', celsius, '도는 화씨', fahrenheit, '도입니다.')
+섭씨 30 도는 화씨 86.0 도입니다.
+fahrenheit=int(input('화씨 온도를 입력하세요:'))
+화씨 온도를 입력하세요:86
+print('화씨', fahrenheit, '도는 섭씨', celsius, '도입니다.')
+화씨 86 도는 섭씨 30 도입니다.
+PI=3.141592
+radius=11
+print('원의 반지름=', radius)
+원의 반지름= 11
+print('원의 둘레=', 2*radius*PI)
+원의 둘레= 69.115024
+print('원의 면적=', radius*radius*PI)
+원의 면적= 380.132632
+radius=int(input('원의 반지름을 입력하세요:'))
+원의 반지름을 입력하세요:11
+print('원의 둘레=', 2*radius*PI)
+원의 둘레= 69.115024
+print('원의 면적=', radius*radius*PI)
+원의 면적= 380.132632
+a=int(input('밑변을 입력하세요:'))
+밑변을 입력하세요:5
+b=int(input('높이를 입력하세요:'))
+높이를 입력하세요:12
+print('빗변:', (a**2+b**2)**0.5)
+빗변: 13.0
