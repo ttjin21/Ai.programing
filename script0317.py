115805f90cbadc0e6c44815cf5b94fb4bd77681f
ttjin21 <dldbwls825@gmail.com>
2025-03-17 02:59:53 +0900

    Add files via upload

---
+++ script0317.py
@@ -0,0 +1,51 @@
+Python 3.11.0b1 (main, May  7 2022, 22:43:46) [MSC v.1931 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('나의 이름은:', '홍길동')
+나의 이름은: 홍길동
+print('나의 나이는:', '27')
+나의 나이는: 27
+print('나의 키는', 179, 'cm입니다.')
+나의 키는 179 cm입니다.
+print('10+20=', 10+20)
+10+20= 30
+print('10*20=', 10*20)
+10*20= 200
+name='전우치'
+print('나의 이름은:', name)
+나의 이름은: 전우치
+age=27
+print('나의 나이는:', age)
+나의 나이는: 27
+height=179
+print('나의 키는', height, 'cm입니다.')
+나의 키는 179 cm입니다.
+sum=10+20
+print('10+20=', sum)
+10+20= 30
+mult=10*20
+print('10*20=', mult)
+10*20= 200
+width=20
+height=40
+width=30
+area=width*height
+print('사각형의 면적', area)
+사각형의 면적 1200
+123*456
+56088
+1357*2468
+3349076
+5**4
+625
+10/4
+2.5
+10//5
+2
+10%5
+0
+5%2
+1
+2**0.5
+1.4142135623730951
+3**(1/3)
+1.4422495703074083
