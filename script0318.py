f0faf87c57dfc42e5d2ba2232d55fca312f2d630
ttjin21 <dldbwls825@gmail.com>
2025-03-17 03:32:04 +0900

    Add files via upload

---
+++ script0318.py
@@ -0,0 +1,27 @@
+Python 3.11.0b1 (main, May  7 2022, 22:43:46) [MSC v.1931 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(200, '+', 300, '+', 400, '=', 900)
+200 + 300 + 400 = 900
+weight=30
+height=60
+print(weight)
+30
+print(height)
+60
+width=30
+height=60
+print('사각형의 면적:', width*height)
+사각형의 면적: 1800
+width=40
+height=20
+print('삼각형의 면적:', width*height/2)
+삼각형의 면적: 400.0
+side=int(input('정사각형의 밑변을 입력하시오:'))
+정사각형의 밑변을 입력하시오:40
+area=side*side
+print('정사각형의 면적:', area)
+정사각형의 면적: 1600
+print('1에서 10까지의 합:', 1+2+3+4+5+6+7+8+9+10)
+1에서 10까지의 합: 55
+print('10!=', 1*2*3*4*5*6*7*8*9*10)
+10!= 3628800
